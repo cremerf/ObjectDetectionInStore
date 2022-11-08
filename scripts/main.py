438698d834d1368fb07b161ff3827f12281cc8b6
cremerf <cremer.federico@gmail.com>
2022-11-08 20:16:17 -0300

    multiprocessing tested

--- scripts/main.py
+++ scripts/main.py
@@ -1,8 +1,10 @@
 from packages.prepare_dataset import main_prepare_datasets
+from packages.prepare_labels import main_prepare_labels
 
 
 def main():
     main_prepare_datasets()
+    main_prepare_labels()
 
 
 
--- scripts/packages/prepare_dataset.py
+++ scripts/packages/prepare_dataset.py
@@ -130,57 +130,6 @@ def split_data_set(DATA_FOLDER: str, OUTPUT_DATA_FOLDER: str, IMAGES_FOLDER: str
                 # Move file
                 os.link(src, dst)
 
-def create_labels_dir(OUTPUT_DATA_FOLDER:str , LABELS_FOLDER:str, subset: str):
-
-    # create path: data/labels
-    labels_folder_path = os.path.join(OUTPUT_DATA_FOLDER, LABELS_FOLDER)
-
-    if not os.path.exists(labels_folder_path):
-        os.makedirs(labels_folder_path)
-
-    # create path: data/labels/train
-    subset_labels_folder_path = os.path.join(labels_folder_path, subset)
-
-    if not os.path.exists(subset_labels_folder_path):
-        os.makedirs(subset_labels_folder_path)
-
-def from_csv_to_list(filename: str, DATA_FOLDER:str, subset: str):
-
-    df_annotations = pd.read_csv(f'{DATA_FOLDER}/annotations_{subset}.csv', names=["image_name", "x1", "y1", "x2", "y2","class", "image_width", "image_height"])
-
-    list_normalize_coord = []
-    starter = 0
-
-    for i in df_annotations.loc[df_annotations['image_name'] == filename].values:
-        
-        b_center_x = (i[1] + i[3]) / 2 
-        b_center_y = (i[2] + i[4]) / 2
-        b_width    = (i[3] - i[1])
-        b_height   = (i[4] - i[2])
-
-        # Normalise the co-ordinates by the dimensions of the image
-        image_w = i[6]
-        image_h= i[7]
-        image_c = i[5]
-        b_center_x /= image_w 
-        b_center_y /= image_h 
-        b_width    /= image_w 
-        b_height   /= image_h
-        
-        starter += 1
-    
-        list_normalize_coord.append("{} {:.3f} {:.3f} {:.3f} {:.3f}".format(starter, b_center_x, b_center_y, b_width, b_height))
-    
-    return list_normalize_coord
-
-def get_txt_normalized_coords(subset:str, subset_labels_folder_path:str, filename:str):
-
-    list_normalize_coord = from_csv_to_list(filename= filename, DATA_FOLDER= DATA_FOLDER, subset= subset)
-
-    path_to_file = os.path.join(subset_labels_folder_path, filename).replace("jpg", "txt")
-
-    with open(path_to_file, 'w') as f:
-        f.write("\n".join(list_normalize_coord))
 
 def get_list_of_filenames(DATA_FOLDER: str, subset: str):
 
@@ -284,50 +233,22 @@ def run():
         BUCKET_PREFIX = BUCKET_PREFIX,
         DATA_FOLDER = DATA_FOLDER)
 
+    # Split dataset into test/train/val
     split_data_set(DATA_FOLDER = DATA_FOLDER, OUTPUT_DATA_FOLDER = OUTPUT_DATA_FOLDER, IMAGES_FOLDER = IMAGES_FOLDER)
 
-    # ----TRAIN SAMPLES----
-    subset_train = 'train'
     # Plot BB in train samples -> sobre datos limpios
+    subset_train = 'train'
     plot_bounding_box(DATA_FOLDER = DATA_FOLDER, OUTPUT_DATA_FOLDER= OUTPUT_DATA_FOLDER, OUTPUT_DATA_BB_FOLDER = OUTPUT_DATA_BB_FOLDER, subset= subset_train)
 
-    # Generate TXT with normalized bounding box coordinates
-    list_of_filenames = get_list_of_filenames(DATA_FOLDER= DATA_FOLDER, subset= subset_train)
-    pool = multiprocessing.Pool()
-    labels_folder_path = os.path.join(OUTPUT_DATA_FOLDER, LABELS_FOLDER)
-    subset_labels_folder_path = os.path.join(labels_folder_path, subset_train)
-    func = partial(get_txt_normalized_coords, subset_train, subset_labels_folder_path)
-    pool.map(func, list_of_filenames)
-    pool.close()
-    pool.join()
-
-    # ----VAL SAMPLES----
-    subset_val = 'val'
     # Plot BB in val samples
+    subset_val = 'val'
     plot_bounding_box(DATA_FOLDER = DATA_FOLDER, OUTPUT_DATA_FOLDER= OUTPUT_DATA_FOLDER, OUTPUT_DATA_BB_FOLDER = OUTPUT_DATA_BB_FOLDER, subset= subset_val)
 
-    # Generate TXT with normalized bounding box coordinates
-    list_of_filenames = get_list_of_filenames(DATA_FOLDER= DATA_FOLDER, subset= subset_val)
-    pool = multiprocessing.Pool()
-    labels_folder_path = os.path.join(OUTPUT_DATA_FOLDER, LABELS_FOLDER)
-    subset_labels_folder_path = os.path.join(labels_folder_path, subset_val)
-    func = partial(get_txt_normalized_coords, subset_val, subset_labels_folder_path)
-    pool.map(func, list_of_filenames)
-    pool.close()
-    pool.join()
-
-    # ----TEST SAMPLES----
-    subset_test = 'test'
     # Plot BB in test samples
+    subset_test = 'test'
     plot_bounding_box(DATA_FOLDER = DATA_FOLDER, OUTPUT_DATA_FOLDER= OUTPUT_DATA_FOLDER, OUTPUT_DATA_BB_FOLDER = OUTPUT_DATA_BB_FOLDER, subset= subset_test)
-    list_of_filenames = get_list_of_filenames(DATA_FOLDER= DATA_FOLDER, subset= subset_val)
-    pool = multiprocessing.Pool()
-    labels_folder_path = os.path.join(OUTPUT_DATA_FOLDER, LABELS_FOLDER)
-    subset_labels_folder_path = os.path.join(labels_folder_path, subset_val)
-    func = partial(get_txt_normalized_coords, subset_val, subset_labels_folder_path)
-    pool.map(func, list_of_filenames)
-    pool.close()
-    pool.join()
+
+
 
 def main_prepare_datasets():
     run()
